9810bbcf624e8c85ff4877faf97b110cf3ad19eb
Rose Kairu <rosekairu@gmail.com>
2020-05-23 00:39:56 +0300

    modified user file

--- user.py
+++ user.py
@@ -1,24 +1,72 @@
+from credential import Credential
+
+
 class User:
     """
     a class that generates new instances of users
     """
     pass
 
-    users_array = []
+    users_list = []
 
     def __init__(self, first_name, last_name):
+        '''
+        __init__ method that helps us define properties for our users.
+        '''
+
         self.first_name = first_name
         self.last_name = last_name
 
-    def save_user_details(self):
+    def save_user(self):
         """
-        save_contact method saves contact objects into user_array
+        save_contact method saves contact objects into user_list
         """
-        User.users_array.append(self)
+        User.users_list.append(self)
+
+    def delete_users(self):
+        """
+        method that deletes a user from the list
+        """
+        User.users_list.remove(self)
+
+    @classmethod
+    def log_in(cls, user_name, password):
+        '''
+        Method that allows a user to log into their credential
+        Args:
+            name : user_name of the user
+            password : password for the user
+        Returns:
+            Credential list for the user that matches the name and password
+            False: if the name or password is incorrect
+        '''
+
+        # Search for the user in the user list
+        for user in User.users_list:
+            if user.user_name == user_name and user.user_password == password:
+                return Credential.credential_list
+
+        return False
 
     @classmethod
     def display_users(cls):
         """
-        method that returns the class array
+        method that returns the list
         """
-        return cls.users_array
+        return User.users_list
+
+    @classmethod
+    def users_exist(cls, name):
+        '''
+        method that checks if a user exists from the user_list
+
+        Args:
+            name: username to search if the person exists
+        Returns:
+        Boolean: true or false depending if the user exists
+        '''
+        for user in User.users_list:
+            if user.user_name == name:
+                return True
+
+        return False
